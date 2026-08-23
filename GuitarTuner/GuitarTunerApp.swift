3d5c3515ae3eed3eabbaec52c73ffbad58f69068
Dominic Legault <toutlemondesencriss@gmail.com>
2026-08-23 10:15:09 -0700

    Initial Commit

---
+++ GuitarTuner/GuitarTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarTunerApp.swift
+//  GuitarTuner
+//
+//  Created by Dominic Legault on 2026-08-23.
+//
+
+import SwiftUI
+
+@main
+struct GuitarTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
